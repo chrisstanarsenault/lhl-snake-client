118558b854d201bb2136019747eebd628809814c
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-09-15 02:30:30 -0400

    created client module and moved connect from play to client

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+/**
+ * Establishes connection with the game server
+ */
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.setTimeout(3000);
+  conn.on("timeout", () => {
+    console.log("you ded cuz you idled");
+    conn.end();
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
