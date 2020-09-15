e5648f92652cac935ce013548a4645d82bf6544a
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-09-15 02:42:10 -0400

    raised idle time from 3 seconds to 5.  Added client name to snake upon connection

--- client.js
+++ client.js
@@ -9,10 +9,15 @@ const connect = function () {
     port: 50541,
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: CSA");
+  });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.setTimeout(3000);
+  conn.setTimeout(5000);
   conn.on("timeout", () => {
     console.log("you ded cuz you idled");
     conn.end();
