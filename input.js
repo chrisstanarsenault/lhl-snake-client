74d6441d723a00bf4875258aa38e51977f276f3d
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-09-15 23:03:23 -0400

    created input.js to house setupInput and HandleUserInput functions to make more moduler

---
+++ input.js
@@ -0,0 +1,32 @@
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (key) => {
+    handleUserInput(key);
+  });
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    return process.exit();
+  } else if (key === "ArrowUp" || key === "w") {
+    return process.stdout.write("Move: up");
+  } else if (key === "ArrowRight" || key === "d") {
+    return process.stdout.write("Move: right");
+  } else if (key === "ArrowDown" || key === "s") {
+    return process.stdout.write("Move: down");
+  } else if (key === "ArrowLeft" || key === "a") {
+    return process.stdout.write("Move: left");
+  }
+};
+
+module.exports = { setupInput };
